0f244da9f7bd7193fff3792d823a08d4048a2e58
MH020 <166133291+MH020@users.noreply.github.com>
2025-09-09 11:33:05 +0200

    stupid db

--- who_knows/app/who_knows.rb
+++ who_knows/app/who_knows.rb
@@ -1,10 +1,16 @@
 require 'sinatra'
 require 'json'
+require_relative '../db/schema'
 
 set :port, 8080
 
 get '/' do
     content_type:json
     {message: "Hello Frank!"}.to_json
+end
+
+get '/pages' do
+  @pages = DB.execute("SELECT * FROM pages")
+  erb :index
 end
 
