275ecef2a45594da8833e737d511cfbcc61710f0
signelystrup <166133407+signelystrup@users.noreply.github.com>
2025-09-15 13:41:43 +0200

    typed up all endpoints

---
+++ who_knows/app/src/controller/controller.rb
@@ -0,0 +1,41 @@
+set :port, 8080
+
+#SERVE HTML PAGES:
+get '/' do
+  # serve root page
+end
+
+get '/weather' do
+  #serve weather page. later.
+end
+
+get '/register' do
+  #serve register page
+end
+
+get '/login' do
+  #serve login page
+end
+
+#API'S
+get '/api/search' do
+  #search
+end
+
+get '/api/weather' do
+  #weather. later.
+end
+
+post '/api/register' do
+  #register
+end
+
+post '/api/login' do
+  #login
+end
+
+get '/api/logout' do
+  #logout.
+end
+
+
