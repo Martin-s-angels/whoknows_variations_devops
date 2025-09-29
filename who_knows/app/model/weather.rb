d54bc773a5badeb1e1aed2cddfb0e8c066c62020
Jasmin <144851658+jazzyzoney@users.noreply.github.com>
2025-09-29 18:34:25 +0000

    latest

--- who_knows/app/model/weather.rb
+++ who_knows/app/model/weather.rb
@@ -8,6 +8,8 @@ def fetch_weather
   api_key = ENV['WEATHER_API_KEY'] #a problem: it refuses to fetch when the api is hidden in .env, but when written raw the data loads ????
   city = "Copenhagen"
   url = "http://api.weatherapi.com/v1/current.json?key=#{api_key}&q=#{city}&aqi=no"
+
+  puts "api key #{url}"
   
   response = HTTParty.get(url)
   if response.success?
