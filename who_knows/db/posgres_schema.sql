083d06f38ec6ae6f36cfec76d606f515fac808cb
signelystrup <166133407+signelystrup@users.noreply.github.com>
2025-12-02 13:49:52 +0100

    Update posgres_schema.sql

--- who_knows/db/posgres_schema.sql
+++ who_knows/db/posgres_schema.sql
@@ -7,10 +7,6 @@ CREATE TABLE IF NOT EXISTS users (
   password TEXT NOT NULL
 );
 
-INSERT INTO users (username, email, password) 
-    VALUES ('admin', 'keamonk1@stud.kea.dk', '5f4dcc3b5aa765d61d8327deb882cf99');
-
-
 DROP TABLE IF EXISTS pages;
 
 CREATE TABLE IF NOT EXISTS pages (
