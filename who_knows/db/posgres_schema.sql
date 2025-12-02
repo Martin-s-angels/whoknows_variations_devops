2c8b3e6b18c3a52b0cdd6814252340111c980fcb
Martin <166133291+MH020@users.noreply.github.com>
2025-12-02 10:22:32 +0100

    added database to our workflows

---
+++ who_knows/db/posgres_schema.sql
@@ -0,0 +1,22 @@
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE IF NOT EXISTS users (
+  id SERIAL PRIMARY KEY,
+  username TEXT NOT NULL UNIQUE,
+  email TEXT NOT NULL UNIQUE,
+  password TEXT NOT NULL
+);
+
+INSERT INTO users (username, email, password) 
+    VALUES ('admin', 'keamonk1@stud.kea.dk', '5f4dcc3b5aa765d61d8327deb882cf99');
+
+
+DROP TABLE IF EXISTS pages;
+
+CREATE TABLE IF NOT EXISTS pages (
+    title TEXT PRIMARY KEY,
+    url TEXT NOT NULL UNIQUE,
+    language TEXT NOT NULL DEFAULT 'en' CHECK (language IN ('en', 'da')),
+    last_updated TIMESTAMP,
+    content TEXT NOT NULL
+);
