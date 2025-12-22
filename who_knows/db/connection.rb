c98fc56920d1538376410eade7b80d7b1a194f8a
Martin <166133291+MH020@users.noreply.github.com>
2025-12-22 20:26:48 +0000

    fix to correct connection in connection.rb

--- who_knows/db/connection.rb
+++ who_knows/db/connection.rb
@@ -4,6 +4,4 @@ Dotenv.load('./dotenv/.env') # environment variables.
 
 puts File.exist?('./dotenv/.env')
 
-def db_conn
-  @db_conn ||= PG.connect(ENV['DB_URI_PROD'])
-end
+DB_CONN = PG.connect(ENV['DB_URI_PROD'])
