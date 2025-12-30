8f0fc238cd679484cf4368a738d052d296dfb811
Martin <166133291+MH020@users.noreply.github.com>
2025-12-30 11:49:51 +0000

    more monitoring and ran migrations and seeds

--- who_knows/knex/knexfile.js
+++ who_knows/knex/knexfile.js
@@ -1,5 +1,9 @@
-import dotenv from 'dotenv'
-dotenv.config({ path: '../dotenv/.env' })
+import path from 'path';
+import dotenv from 'dotenv';
+
+dotenv.config({
+  path: path.resolve(process.cwd(), '../dotenv/.env'),
+});
 
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
@@ -46,17 +50,14 @@ export default {
         migrations: { tableName: 'knex_migrations' }
     },
 
-    production: { //if db hosted on other server
-        client: 'pg',
-        connection: {
-            host: '127.0.0.1', //change to server url
-            port: 5432,
-            user: process.env.POSTGRES_USER,
-            password: process.env.POSTGRES_PASSWORD,
-            database: process.env.POSTGRES_DB
-        },
-        pool: { min: 2, max: 10 },
-        migrations: { tableName: 'knex_migrations' }
+  production: {
+    client: 'pg',
+    connection: {
+      connectionString: process.env.DB_URI_PROD,
+      ssl: false
     },
+    pool: { min: 2, max: 10 },
+    migrations: { tableName: 'knex_migrations' }
+  }
 
 }
